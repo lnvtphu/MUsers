be589cbad4e4d73c8f8de2a7f9aa85b7d191822d
lnvtphu <lnvtphu@gmail.com>
2016-06-22 16:05:26 +0700

    2206

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-console.clear();
 
 var UsersList = React.createClass({
     render: function(){
         return <ul>
-            {this.props.users.map((user, userIndex) =>
+            {this.props.users.map((templeUser, userIndex) =>
                 <li key={userIndex}>
-                    <img src = {user.photo} width = '70' height = '70' />
-                    {user.firstName}
+                    <img src = {templeUser.photo} width = '70' height = '70' />
+                    {templeUser.firstName} {templeUser.lastName}{templeUser.id}
                     <button onClick={this.props.deleteUser} value={userIndex}> Delete </button>
+                    <button onClick={this.props.editUser} value ={userIndex}> Edit</button>
                 </li>
             )}
         </ul>;
@@ -15,67 +15,140 @@ var UsersList = React.createClass({
  });
 
 var AppMUser = React.createClass({
+    propTypes: function() {
+      numberUser: React.PropTypes.number.isRequired
+      indexUser: React.PropTypes.number.isRequired
+    },
     getInitialState: function(){
+        var templeUser = {
+            "id": "",
+            "firstName": "",
+            "lastName": "",
+            "photo": "https://about.udemy.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png",
+            "friends": []
+        }
+        var users = [{
+            "id": "1",
+            "firstName": "Tom",
+            "lastName": "Cruise",
+            "photo": "http://cdn2.gossipcenter.com/sites/default/files/imagecache/story_header/photos/tom-cruise-020514sp.jpg",
+            "friends": [2, 3]
+        }, {
+            "id": "2",
+            "firstName": "Maria",
+            "lastName": "Sharapova",
+            "photo": "http://thewallmachine.com/files/1363603040.jpg",
+            "friends": [1]
+        }, {
+            "id": "3",
+            "firstName": "James",
+            "lastName": "Bond",
+            "photo": "http://georgesjournal.files.wordpress.com/2012/02/007_at_50_ge_pierece_brosnan.jpg",
+            "friends": [2]
+        }];
         return {
-          users : [{
-              "id": "1",
-              "firstName": "Tom",
-              "lastName": "Cruise",
-              "photo": "http://cdn2.gossipcenter.com/sites/default/files/imagecache/story_header/photos/tom-cruise-020514sp.jpg",
-              "friends": [2, 3]
-          }, {
-              "id": "2",
-              "firstName": "Maria",
-              "lastName": "Sharapova",
-              "photo": "http://thewallmachine.com/files/1363603040.jpg",
-              "friends": [1]
-          }, {
-              "id": "3",
-              "firstName": "James",
-              "lastName": "Bond",
-              "photo": "http://georgesjournal.files.wordpress.com/2012/02/007_at_50_ge_pierece_brosnan.jpg",
-              "friends": [2],
-              templeUser: ''
-          }]
+          users,
+          templeUser,
+          numberUser: users.length,
+          indexUser:' '
         }
     },
 
     deleteUser: function(e) {
         var userIndex = parseInt(e.target.value);
+        var userID = this.state.numberUser - 1;
         this.setState(state => {
             state.users.splice(userIndex, 1);
-            return {users: state.users};
+            return {
+                users: state.users,
+                numberUser: userID
+            };
         });
     },
-
-    onChange: function(e) {
-        this.setState({ user: e.target.value });
+    editUser: function(e){
+        var userEdit = this.state.users[parseInt(e.target.value)];
+        console.log(userEdit);
+        this.setState({ templeUser: userEdit, indexUser: parseInt(e.target.value) });
     },
-
-
+    onChangeFirtname: function(e) {
+            var tFirtname = React.addons.update(this.state.templeUser,{
+                firstName: {$set: e.target.value },
+                id: {$set: this.state.numberUser +1}
+              });
+        this.setState({
+            templeUser: tFirtname
+        });
+    },
+    onChangeLastname: function(e){
+            var tLastname = React.addons.update(this.state.templeUser,{
+                lastName: {$set: e.target.value}
+            });
+            this.setState({ templeUser: tLastname });
+    },
 
     addUser:function (e){
-        this.setState({
-            users: this.state.users.concat([this.state.templeUser]),
 
-        })
-
+        if(this.state.templeUser.firstName!='' && this.state.templeUser.lastName!=''){
+            var userID = this.state.numberUser +1;
+            var tUser = React.addons.update(this.state.templeUser,{
+                id: {$set: ''},
+                firstName : {$set: ''},
+                lastName : {$set: ''}
+            });
+            this.setState({
+                numberUser: userID,
+                users: this.state.users.concat([this.state.templeUser]),
+                templeUser: tUser
+            });
+            console.log(this.state.users);
+        }else {
+            alert('Some field null!');
+        }
         e.preventDefault();
     },
+    updateUser: function(e){
+        if(this.state.templeUser.firstName!='' && this.state.templeUser.lastName!=''){
+            var tUser = React.addons.update(this.state.templeUser,{
+                id: {$set: ''},
+                firstName : {$set: ''},
+                lastName : {$set: ''}
+            });
+            this.setState(state => {
+                state.users.splice(this.state.indexUser, 1, this.state.templeUser);
+                return {
+                    users: state.users,
+                    templeUser: tUser
+                };
+            });
+        }else {
+            alert('Some field null!');
+        }
+        e.preventDefault();
 
+
+    },
     render: function(){
         return(
             <div>
+                <input  placeholder="firstName" onChange={this.onChangeFirtname} type="text" value={this.state.templeUser.firstName}/>
+                <input  placeholder="lastName" onChange={this.onChangeLastname} type="text" value={this.state.templeUser.lastName}/>
+                <button onClick={this.addUser}> Add User </button>
+                <button onClick={this.updateUser}> Update User</button>
                 <h1>LIST USER </h1>
-                <UsersList users={this.state.users} deleteUser={this.deleteUser} />
+                <UsersList users={this.state.users} deleteUser={this.deleteUser} editUser={this.editUser}/>
 
-                <form onSubmit={this.addUser}>
-                    <input onChange={this.onChange} type="text" value={this.state.user}/>
-                    <button> Add Name User </button>
-                </form>
             </div>
         );
     }
 });
-
+var ListFriends = React.createClass({
+    render: function(){
+        return<ul>
+            {this.props.users.map((templeUser, userIndex) =>
+                <li key={userIndex}>
+                </li>
+            )}
+        </ul>;
+    }
+});
 React.render(<AppMUser />, document.getElementById('appMUser'));
