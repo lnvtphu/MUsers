75dbd2c33f995efefbc7ddb88cb298baeb308de9
lnvtphu <lnvtphu@gmail.com>
2016-06-23 18:02:13 +0700

    2306l3

--- js/app.js
+++ js/app.js
@@ -1,33 +1,10 @@
 
-var UsersList = React.createClass({
-    render: function(){
-        return <ul className='list-group listUser'>
-            {this.props.users.map((templeUser, userIndex) =>
-                <li className='list-group-item' key={userIndex}>
-                    <div>
-                        <img src = {templeUser.photo} width = '70' height = '70' />
-                        {templeUser.firstName}
-                    </div>
-                    <div>
-                        <button onClick={this.props.deleteUser} value={userIndex}> Delete </button>
-                        <button onClick={this.props.editUser} value={userIndex}> Edit</button>
-                        <button onClick={this.props.makeFriends} value={userIndex}> List Friend</button>
-                    </div>
-                </li>
-            )}
-        </ul>;
-    }
- });
-
 var AppMUser = React.createClass({
     propTypes: function() {
       numberUser: React.PropTypes.number.isRequired
       indexUser: React.PropTypes.number.isRequired
     },
     getInitialState: function(){
-        var divListFriend = {
-            marginTop: '0'
-        }
         var templeUser = {
             "id": "",
             "firstName": "",
@@ -59,11 +36,10 @@ var AppMUser = React.createClass({
           templeUser,
           numberUser: users.length,
           indexUser: 0,
-          friendsArr: [],
-          divListFriend
+          friendsArr: []
         }
     },
-
+    //delete user on list users
     deleteUser: function(e) {
         var userIndex = parseInt(e.target.value);
         var userID = this.state.numberUser - 1;
@@ -75,11 +51,13 @@ var AppMUser = React.createClass({
             };
         });
     },
+    //view info of user select
     editUser: function(e){
         var userEdit = this.state.users[parseInt(e.target.value)];
         console.log(userEdit);
         this.setState({ templeUser: userEdit, indexUser: parseInt(e.target.value) });
     },
+    //event set state when change text field lastName
     onChangeFirtname: function(e) {
             var tFirtname = React.addons.update(this.state.templeUser,{
                 firstName: {$set: e.target.value },
@@ -89,16 +67,20 @@ var AppMUser = React.createClass({
             templeUser: tFirtname
         });
     },
+    //event set state when change text field lastName
     onChangeLastname: function(e){
             var tLastname = React.addons.update(this.state.templeUser,{
                 lastName: {$set: e.target.value}
             });
             this.setState({ templeUser: tLastname });
     },
-
+    //event add new user
     addUser:function (e){
 
-        if(this.state.templeUser.firstName!='' || this.state.templeUser.lastName!=''){
+        if(this.state.templeUser.firstName=='' || this.state.templeUser.lastName==''){
+            alert('Some field null!');
+            console.log(this.state.users);
+        }else {
             var userID = this.state.numberUser +1;
             var tUser = React.addons.update(this.state.templeUser,{
                 id: {$set: ''},
@@ -111,12 +93,10 @@ var AppMUser = React.createClass({
                 users: this.state.users.concat([this.state.templeUser]),
                 templeUser: tUser
             });
-            console.log(this.state.users);
-        }else {
-            alert('Some field null!');
         }
         e.preventDefault();
     },
+    //event update info user
     updateUser: function(e){
         if(this.state.templeUser.firstName=='' || this.state.templeUser.lastName==''){
             alert('Some field null!');
@@ -138,6 +118,7 @@ var AppMUser = React.createClass({
         }
 
     },
+    //delete friend on list
     deleteFriend: function(e){
         var userIndex = parseInt(e.target.value);
         this.setState(state => {
@@ -148,28 +129,29 @@ var AppMUser = React.createClass({
             };
         });
     },
-    makeFriends: function(e){
+    //event click view list friend of user
+    viewFriends: function(e){
             var friendCustomArr = [];
-            var divStyle = React.addons.update(this.state.divListFriend,{
-                marginTop:{$set: (e.target.value)*50}
-            });
             var idFriendArr = this.state.users[e.target.value].friends;
-            for(i = 0; i<this.state.numberUser; i++ ){
+            for(i = 0; i<this.state.users.length; i++ ){
                 for(j = 0; j<idFriendArr.length; j++){
                     if(idFriendArr[j]==this.state.users[i].id)
                     {
                         var friend = {
                             'id': idFriendArr[j],
                             'name': this.state.users[i].firstName
-                        }
+                        };
+
                         friendCustomArr.push(friend);
                     }
                 }
             }
+
             this.setState({
-                friendsArr: friendCustomArr,
-                divListFriend: divStyle
+                friendsArr: friendCustomArr
             });
+            console.log(e.target.value);
+            console.log(friendCustomArr);
     },
     render: function(){
 
@@ -188,17 +170,38 @@ var AppMUser = React.createClass({
                       <button onClick={this.updateUser}> Update User</button>
                   </div>
                   <div className='leftStyle'>
-                      <UsersList users={this.state.users} deleteUser={this.deleteUser} editUser={this.editUser} makeFriends={this.makeFriends}/>
+                      <UsersList users={this.state.users} deleteUser={this.deleteUser} editUser={this.editUser} viewFriends={this.viewFriends}/>
                   </div>
                   <div className='rightStyle'>
-                      <ListFriends friendsArr={this.state.friendsArr} deleteFriend={this.deleteFriend}/>
+                      <FriendsList friendsArr={this.state.friendsArr} deleteFriend={this.deleteFriend}/>
                  </div>
                  <div className='clear'></div>
             </div>
         );
     }
 });
-var ListFriends = React.createClass({
+//create class list user
+var UsersList = React.createClass({
+    render: function(){
+        return <ul className='list-group listUser'>
+            {this.props.users.map((templeUser, userIndex) =>
+                <li className='list-group-item' key={userIndex}>
+                    <div>
+                        <img src = {templeUser.photo} width = '70' height = '70' />
+                        <span>{templeUser.firstName}</span>
+                    </div>
+                    <div>
+                        <button onClick={this.props.deleteUser} value={userIndex}> Delete </button>
+                        <button onClick={this.props.editUser} value={userIndex}> Edit</button>
+                        <button onClick={this.props.viewFriends} value={userIndex}> List Friend</button>
+                    </div>
+                </li>
+            )}
+        </ul>;
+    }
+ });
+//create class list friend
+var FriendsList = React.createClass({
     render: function(){
         return <ul className='list-group'>
             {this.props.friendsArr.map((templeFriend, friendIndex) =>
